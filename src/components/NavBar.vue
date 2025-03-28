fa7a61fa9896c86343e1759f63bb46fc979988c0
shez <shezcode@gmail.com>
2025-03-28 11:08:29 +0100

    navbar with vuetify + changed theme

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,21 +1,46 @@
-<script setup lang="ts">
-import { RouterLink } from 'vue-router';
-</script>
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/products">Products</RouterLink>
-    <RouterLink to="/categories">Categories</RouterLink>
-  </nav>
+  <v-app-bar color="primary" dark app>
+    <v-app-bar-title>
+      <span class="font-weight-bold">ECOM</span>
+    </v-app-bar-title>
+
+    <div class="d-flex align-center justify-around">
+      <v-btn v-for="item in navItems" :key="item.title" :to="item.to" class="mx-2" variant="text">
+        <v-icon start :icon="item.icon"></v-icon>
+        {{ item.title }}
+      </v-btn>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon icon="mdi-cart"></v-icon>
+      <v-badge
+        v-if="cartItemCount > 0"
+        :content="cartItemCount"
+        color="error"
+        offset-x="12"
+        offset-y="12"
+      ></v-badge>
+    </v-btn>
+
+    <v-btn icon class="ml-2">
+      <v-icon icon="mdi-account"></v-icon>
+    </v-btn>
+  </v-app-bar>
 </template>
 
-<style scoped>
-nav {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
-  width: 100%;
-}
-</style>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// Navigation items with icons
+const navItems = [
+  { title: 'Home', to: '/', icon: 'mdi-home' },
+  { title: 'Products', to: '/products', icon: 'mdi-shopping' },
+  { title: 'Categories', to: '/categories', icon: 'mdi-shape' },
+  { title: 'About', to: '/about', icon: 'mdi-information' },
+];
+
+// Simulated cart items count (you'll connect this to your store later)
+const cartItemCount = ref(3);
+</script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,8 +2,31 @@
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
 
-// Vuetify
-import { createVuetify } from 'vuetify';
+import { createVuetify, type ThemeDefinition } from 'vuetify';
 
-export default createVuetify();
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+const darkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: '#1E1E2E',
+    surface: '#2A2A3C',
+    primary: '#A2E1B8',
+    'primary-darken-1': '#80D090',
+    secondary: '#9DDCD2',
+    'secondary-darken-1': '#7BCBC0',
+    error: '#F5A9B8',
+    info: '#B1D0F7',
+    success: '#B8E0C5',
+    warning: '#FFD6AA',
+    accent: '#D4BBEB',
+    'on-background': '#E9E9F4',
+    'on-surface': '#E9E9F4',
+  },
+};
+export default createVuetify({
+  theme: {
+    defaultTheme: 'darkTheme',
+    themes: {
+      darkTheme,
+    },
+  },
+});
